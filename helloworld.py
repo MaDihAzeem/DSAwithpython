9fc82f22f8f443784d95c59c57bfca5d140f0d6d
MaDihAzeem <azeemmadiha41@gmail.com>
2023-03-07 12:46:51 +0500

    my profile

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,11 @@
-print(first day of python)
+print( "\t" "MY PROFILE"  )
+Name="Name: Madiha Azeem"                             
+Class="class: Batch 02 RS&GIS"
+Rollno="Rollno: 211101009"
+Degree="Degree: Remote Sensing And Geo informatics"
+Age="Age: 20"
+print(Name)
+print(Class)
+print(Rollno)
+print(Degree)
+print(Age)
