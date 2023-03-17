750bf83ac8e141bd4dc76179f57dc94188b2fd92
MaDihAzeem <azeemmadiha41@gmail.com>
2023-03-17 12:37:08 +0500

    list

---
+++ list.py
@@ -0,0 +1,22 @@
+list1=['Madiha','Munir','Mubashir','jawad']
+list1[0]='Aman'
+list1.append('faiza')
+list1.insert(3,'Amana')
+does_exist='jawad'in list1
+print(does_exist)
+does_exist='kiran'in list1
+print(does_exist)
+print('Name:', list1[-3])
+print('Name:', list1[1:4])
+print('Name:', list1)
+print('print number of names:',len(list1))
+student=[['Madiha','211101009'],'ist',2023]
+student.reverse()
+print('Student_info:', student)
+list=list("fatima")
+print(list)
+print[student[2][0]]
+language=['cpp','python','java','html']
+print(language)
+del language
+print(language)
