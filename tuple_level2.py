5b4dd875ff8627413c1110d3aeb879ba47089fad
Madiha Azeem <azeemmadiha41@gmail.com>
2023-04-13 01:25:29 +0500

    tuple_level2

---
+++ tuple_level2.py
@@ -0,0 +1,14 @@
+fruits=('banana','watermelon','apple','avacado')
+vegetables=('onion','potato','tomato','lemon')
+animals=('cat','lion','dog','mouse')
+food_stuf_tp=fruits+vegetables+animals
+print(food_stuf_tp)
+middle_items=food_stuf_tp[4:6]
+print(middle_items)
+del food_stuf_tp
+nordic_countries = ('Denmark', 'Finland','Iceland', 'Norway', 'Sweden')
+print(nordic_countries)
+does_exist= 'Estonia' in nordic_countries
+print(does_exist)
+does_exist= 'Iceland' in nordic_countries
+print(does_exist)
