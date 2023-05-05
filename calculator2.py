b48f54c6a2130ad992517ee01df62af8e6860097
MaDihAzeem <azeemmadiha41@gmail.com>
2023-05-05 12:03:05 +0500

    bubblesort

---
+++ calculator2.py
@@ -0,0 +1,20 @@
+import calculator
+obj = calculator()
+choice = 1
+while choice != 0:
+    print("1. ADD")
+    print("2. SUB")
+    print("3. MUL")
+    print("4. DIV")
+    choice = int(input("Enter your choice:"))
+
+    if choice == 1:
+        print(obj.addition())
+    elif choice == 2:
+        print(obj.subtraction())
+    elif choice == 3:
+        print(obj.multiplication())
+    elif choice == 4:
+        print(obj.division())
+    else:
+        print("Invalid choice")
